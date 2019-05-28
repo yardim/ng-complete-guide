d247c1e84659016ed1f1f94057cdf7c3ffa5ca73
Dmytro Yaroshenko <dmytro.yaroshenko@do.local>
2019-05-28 18:04:03 +0300

    Section 2 - Lecture 20 - working with styles

---
+++ my-first-app/src/app/server/server.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styles: [`
+    p {
+      color: dodgerblue;
+    }
+  `]
+})
+export class ServerComponent {
+
+}
