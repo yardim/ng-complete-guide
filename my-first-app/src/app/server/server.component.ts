b58406835b637566108011fd9de5e92cc5aded47
Dmytro Yaroshenko <dmytro.yaroshenko@do.local>
2019-06-03 13:43:52 +0300

    Section 2 - Lecture 39 - ngFor directive

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -3,13 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styles: [`
-    p {
-      color: dodgerblue;
-    }
-  `]
 })
 export class ServerComponent {
   id = 10;
   status = 'offline';
+
+  constructor() {
+    this.status = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
+  getColor() {
+    return this.status === 'online' ? 'green' : 'red';
+  }
 }
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -9,6 +9,8 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'no server was created';
   allowNewServer = false;
   serverName = 'Test Server';
+  isServerCreated = false;
+  servers = ['Server 1', 'Server 2'];
 
   constructor() { }
 
@@ -19,6 +21,8 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.servers.push(this.serverName);
+    this.isServerCreated = true;
     this.serverCreationStatus = 'server created - ' + this.serverName;
   }
 }
