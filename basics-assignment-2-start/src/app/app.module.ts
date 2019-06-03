15be69c388d3aaa5b8ad975b1e76414da5cc204b
Dmytro Yaroshenko <dmytro.yaroshenko@do.local>
2019-06-03 13:08:38 +0300

    Section 2 - Lecture 33 - Assignment 2 - data binding practice

--- basics-assignment-2-start/src/app/app.module.ts
+++ basics-assignment-2-start/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, UserComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
