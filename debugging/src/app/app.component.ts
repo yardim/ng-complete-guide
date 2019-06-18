2007df15f5c50b8393373478f226d4ad756fcffd
yardim <yaroshenko@bluefountainmedia.com>
2019-06-18 22:34:55 +0300

    Section 4 - Lecture 61 - Augury introduction

--- debugging/src/app/app.component.ts
+++ debugging/src/app/app.component.ts
@@ -6,14 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  servers;
+  servers = [];
 
   onAddServer() {
-    this.servers.push('Another Server');
+    this.servers.push('Another Server ' + (this.servers.length + 1));
   }
 
-  onRemoveServer(id: number) {
-    const position = id + 1;
-    this.servers.splice(position, 1);
+  onRemoveServer(i: number) {
+    this.servers.splice(i, 1);
   }
 }
