31c262bb0503e45e7a4bce22c5035c91c57b5a26
Dmitry Yaroshenko <dmitryy.yaroshenko@dataart.com>
2019-06-24 10:17:23 +0300

    Section 8 - Lecture 9 - closing dropdown from anywhere

--- recipe-book/src/app/app.module.ts
+++ recipe-book/src/app/app.module.ts
@@ -6,19 +6,19 @@ import { AppComponent } from './app.component';
 import { RecipeBookModule } from './recipe-book/recipe-book.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { HeaderComponent } from './header/header.component';
-import { DropdownDirective } from './shared/dropdown.directive';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    DropdownDirective
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     RecipeBookModule,
     ShoppingListModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
