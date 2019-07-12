7bd903dac761060f240b45a171509edbee52e0d4
yardim <yaroshenko@bluefountainmedia.com>
2019-07-12 09:57:14 +0300

    Section 15 - Lecture 191 - resetting forms

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -6,12 +6,53 @@ import { NgForm } from '@angular/forms';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  defaultQuestion = 'pet';
+  answer = '';
+  genders = ['male', 'female'];
+  user = {
+    name: '',
+    mail: '',
+    question: '',
+    answer: '',
+    gender: '',
+  };
+  isSubmitted = false;
+  @ViewChild('f', { static: true }) form: NgForm;
+
+  ngOnInit() {
+    console.log(this.form);
+  }
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    // this.form.setValue({
+    //   answer: '',
+    //   gender: 'male',
+    //   secret: 'pet',
+    //   userData: {
+    //     email: '',
+    //     username: suggestedName
+    //   }
+    // });
+
+    this.form.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
   }
 
   onSubmit(form: NgForm) {
-    console.log(form.value);
+    this.isSubmitted = true;
+
+    this.user.name = form.value.userData.username;
+    this.user.mail = form.value.userData.email;
+    this.user.question = form.value.secret;
+    this.user.answer = form.value.answer;
+    this.user.gender = form.value.gender;
+
+    this.form.reset();
   }
 }
