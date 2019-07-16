8c80501dbcd0463411b3880b7c9451dfa14be54a
yaroshenko.dmitriy <yaroshenko.dmitriy@gmail.com>
2019-07-16 08:53:52 +0300

    Section 15 - Assignment 7

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectForm: FormGroup;
+  projectStatuses = ['stable', 'critical', 'finished'];
+
+  ngOnInit() {
+    this.projectForm = new FormGroup({
+      projectName: new FormControl('', [Validators.required, this.forbiddenName]),
+      email: new FormControl('', [Validators.required, Validators.email], this.forbiddenMail),
+      projectStatus: new FormControl(this.projectStatuses[0]),
+    });
+  }
+
+  forbiddenName(control: FormControl) {
+    if (control.value === 'Test') {
+      return { forbidden: true };
+    }
+
+    return null;
+  }
+
+  forbiddenMail(control: FormControl) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({ forbiddenMail: true });
+        }
+
+        resolve(null);
+      }, 2000);
+    });
+  }
+
+  onSubmit() {
+    console.log(this.projectForm);
+  }
 }
